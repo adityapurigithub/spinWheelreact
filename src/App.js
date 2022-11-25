cd722a78c2aab084722c6dc9df33ff9c74e5ca9f
ADITYA <adityapuri85@gmail.com>
2022-11-25 16:02:16 +0530

    Dome

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [spin, setSpin] = useState("");
+
+  const handleSpin = () => {
+    setSpin("spin__start");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className=""></div>
+      <div class="circle__main">
+        <button className="spinBtn" onClick={handleSpin}>
+          SPIN
+        </button>
+        <div class={`circle__outer ${spin}`}>
+          <div class="poly1">1</div>
+          <div class="poly2">2</div>
+          <div class="poly3">3</div>
+          <div class="poly4">4</div>
+          <div class="poly5">5</div>
+        </div>
+      </div>
     </div>
   );
 }
